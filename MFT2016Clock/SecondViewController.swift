7f8099a5ea2c5c54ccac3bb1a2d44ca725677844
梶原侑馬 <youma0228@softbank.ne.jp>
2016-07-19 10:16:28 +0900

    Initial Commit

---
+++ MFT2016Clock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MFT2016Clock
+//
+//  Created by KajiharaYuma on 2016/07/19.
+//  Copyright © 2016年 KajiharaYuma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
